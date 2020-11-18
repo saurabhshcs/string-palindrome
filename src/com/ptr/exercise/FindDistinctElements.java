def2678bc13fe196a47b8f4d6ced2f61e156d3df
Saurabh Sharma <saurabhshcs@gmail.com>
2020-11-18 23:56:59 +0000

    Refactor class name per it's behavior.

--- src/com/ptr/exercise/FindDistinctElements.java
+++ src/com/ptr/exercise/FindDistinctElements.java
@@ -8,11 +8,10 @@ package com.ptr.exercise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Rextester {
+public class FindDistinctElements {
 
     public static void main(String[] args) {
 
